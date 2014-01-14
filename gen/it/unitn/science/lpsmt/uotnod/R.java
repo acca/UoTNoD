2ffaf6d4150a29fb37a4fcba495a62eb23d39a7f
Daniele Santoro <psyacca@gmail.com>
2014-01-14 09:35:30 +0100

    Added refresh icons resources and item on the Action bar

--- gen/it/unitn/science/lpsmt/uotnod/R.java
+++ gen/it/unitn/science/lpsmt/uotnod/R.java
@@ -21,11 +21,12 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_refresh=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
+        public static final int action_refresh=0x7f080001;
         public static final int action_settings=0x7f080000;
-        public static final int action_update=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -42,13 +43,13 @@ public final class R {
         public static final int uotnod_shops=0x7f070004;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
+        public static final int action_refresh=0x7f050006;
+        public static final int action_settings=0x7f050005;
         public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int title_activity_outnod_weather=0x7f050005;
-        public static final int title_activity_uotnod_family=0x7f050003;
-        public static final int title_activity_uotnod_shops=0x7f050004;
-        public static final int update=0x7f050006;
+        public static final int hello_world=0x7f050001;
+        public static final int title_activity_outnod_weather=0x7f050004;
+        public static final int title_activity_uotnod_family=0x7f050002;
+        public static final int title_activity_uotnod_shops=0x7f050003;
     }
     public static final class style {
         /** 
